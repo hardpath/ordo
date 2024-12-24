d45d8f0d71511f2ea4c28d48c18d6bba28e9434b
Emanuel Oliveira <eoliver@hardpath.co.uk>
2024-12-24 13:55:47 +0000

    Initial setup: Added project structure, .gitignore, and test code.

---
+++ Models/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordo.Models
+{
+    internal class AppSettings
+    {
+        public string? ClientId { get; set; }
+        public string? TenantId { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
